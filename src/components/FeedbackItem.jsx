02eaa18d3a772eb35475366376aa22a4c2d68655
bryce <76966016+brycegallo@users.noreply.github.com>
2023-03-25 17:46:49 -0400

    Implement Feedback Items use states and components to implement

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,20 +1,8 @@
-import { useState } from "react"
-
-function FeedbackItem() {
-    const [rating, setRating] = useState(10)
-    const [text, setText] = useState('Feedback Item example')
-
-    const handleClick = () => {
-        setRating((prev) => {
-            return prev + 1
-        })
-    }
-
+function FeedbackItem({ item }) {
   return (
     <div className="card">
-        <div className="num-display">{rating}</div>
-        <div className="text-display">{text}</div>
-        <button onClick={handleClick}>Click</button>
+        <div className="num-display">{item.rating}</div>
+        <div className="text-display">{item.text}</div>
     </div>
   )
 }
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'default sample feedback text 1'
+    },
+    {
+        id: 2,
+        rating: 7,
+        text: 'default sample feedback text 2'
+    },
+    {
+        id: 3,
+        rating: 9,
+        text: 'default sample feedback text 3'
+    },
+]
+
+export default FeedbackData
