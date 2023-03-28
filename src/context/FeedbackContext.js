61b3eeab5d5bd1da3237c16fc513909f5fc42947
bryce <76966016+brycegallo@users.noreply.github.com>
2023-03-28 14:13:27 -0400

    Implement JSON server allow deletion from JSON database with REST API

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,36 +1,42 @@
-import { v4 as uuidv4 } from 'uuid'
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: 'This is feedback item 1',
-            rating: 10,
-        },
-        {
-            id: 2,
-            text: 'This is feedback item 2',
-            rating: 7,
-        },
-        {
-            id: 3,
-            text: 'This is feedback item 3',
-            rating: 4,
-        },
-    ])
+    const [isLoading, setIsLoading] = useState(true)
+    const [feedback, setFeedback] = useState([])
 
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     })
 
+    useEffect(() => {
+        fetchFeedback()
+    }, [])
+
+    // fetch feedback
+    const fetchFeedback = async () => {
+        const response = await fetch('/feedback?_sort=id_order=desc')
+        const data = await response.json()
+
+        setFeedback(data)
+        setIsLoading(false)
+    }
+
     // add feedback to list
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback, ...feedback])
+    const addFeedback = async (newFeedback) => {
+        const response = await fetch('/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newFeedback),
+        })
+
+        const data = await response.json()
+
+        setFeedback([data, ...feedback])
     }
 
     // delete selected feedback from list
@@ -56,6 +62,7 @@ export const FeedbackProvider = ({ children }) => {
     return <FeedbackContext.Provider value={{
         feedback, // shorthand for feedback: feedback
         feedbackEdit, // piece of state needed for editFeedback function
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback, // function
