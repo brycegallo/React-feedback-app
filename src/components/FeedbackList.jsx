6c1550bb4c7463e28c91e0ce092d584da8ed3bef
bryce <76966016+brycegallo@users.noreply.github.com>
2023-03-27 21:58:43 -0400

    Clean Up App.js with Context use Context to remove clutter from App.js

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,8 +1,11 @@
 import {motion, AnimatePresence} from 'framer-motion'
-import PropTypes from 'prop-types'
+import { useContext } from 'react'
 import FeedbackItem from "./FeedbackItem"
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackList({ feedback, handleDelete }) {
+function FeedbackList() {
+    const {feedback} = useContext(FeedbackContext)
+    
     if(!feedback || feedback.length === 0) {
         return <p>No feedback yet</p>
     }
@@ -17,10 +20,10 @@ function FeedbackList({ feedback, handleDelete }) {
                         animate={{ opacity: 1 }}
                         exit={{ opacity: 0 }}
                     >
-                        <FeedbackItem 
+                    <FeedbackItem 
                         key={item.id} 
                         item={item} 
-                        handleDelete={handleDelete}/>
+                    />
                     </motion.div>
                 ))}
             </AnimatePresence>
@@ -38,16 +41,6 @@ function FeedbackList({ feedback, handleDelete }) {
 //         ))}
 //     </div>
 //   )
-}
-
-FeedbackList.propTypes = {
-    feedback: PropTypes.arrayOf(
-        PropTypes.shape({
-            // id: PropTypes.number.isRequired, // uuid implementation breaks this
-            text: PropTypes.string.isRequired,
-            rating: PropTypes.number.isRequired,
-        })
-    ),
 }
 
 export default FeedbackList
